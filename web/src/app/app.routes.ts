d335b038d2223f61a2cd2f932afdd797b792edd2
Vedran <bajic.sv10.2023@uns.ac.rs>
2025-12-20 12:49:31 +0100

    add: initial components for team

--- web/src/app/app.routes.ts
+++ web/src/app/app.routes.ts
@@ -1,8 +1,5 @@
 import { Routes } from '@angular/router';
-import { HelloComponent } from './hello/hello.component';
+
 
 export const routes: Routes = [
-  { path: 'hello', component: HelloComponent },
-  { path: '', redirectTo: '/hello', pathMatch: 'full' },
-  { path: '**', redirectTo: '/hello'}
 ];
