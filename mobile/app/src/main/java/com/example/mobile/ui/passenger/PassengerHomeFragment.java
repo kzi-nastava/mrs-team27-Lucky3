448a9b1cc61ec9d01e5738cfc8131bd7d17ad3d3
vedranbajic4 <bajic196@gmail.com>
2026-06-15 00:12:20 +0200

    better message UI

--- mobile/app/src/main/java/com/example/mobile/ui/passenger/PassengerHomeFragment.java
+++ mobile/app/src/main/java/com/example/mobile/ui/passenger/PassengerHomeFragment.java
@@ -170,9 +170,10 @@ public class PassengerHomeFragment extends Fragment {
     }
 
     private void showBlockedDialog(String reason) {
+        String messageHtml = "You are currently <b>BLOCKED</b> and cannot order rides.<br><br><b><font color='#F44336'>Reason: " + reason + "</font></b>";
         new MaterialAlertDialogBuilder(requireContext(), R.style.DarkDialogTheme)
-                .setTitle("Access Restricted")
-                .setMessage("YOU ARE BLOCKED YOU CANT ORDER RIDE : " + reason)
+                .setTitle("Usage Restriction")
+                .setMessage(android.text.Html.fromHtml(messageHtml, android.text.Html.FROM_HTML_MODE_COMPACT))
                 .setPositiveButton("OK", (dialog, which) -> dialog.dismiss())
                 .show();
     }
